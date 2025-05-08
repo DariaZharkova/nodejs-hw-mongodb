100f816b0aa0b379fe98259317f9cc37511f5f16
Daria Zharkova <dariazharkova1991@gmail.com>
2025-05-08 11:19:27 +0300

    add error middelware

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
 
+  app.use(express.json());
   app.use(cors());
 
   app.use(
@@ -20,34 +21,52 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
+  app.get('/contacts', async (req, res, next) => {
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      next(error);
+    }
   });
 
   app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
 
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
+      if (!contact) {
+        res.status(404).json({
+          message: 'Contact not found',
+        });
+        return;
+      }
+
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
       });
-      return;
+    } catch (error) {
+      next(error);
     }
-
-    res.status(200).json({
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
   });
 
   app.use((req, res, next) => {
     res.status(404).json({
       message: 'Not found',
+    });
+  });
+
+  app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: error.message,
     });
   });
 
