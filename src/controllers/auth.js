3e62dd92119f4cf7a95e11c0d49397ecf0f06628
Daria Zharkova <dariazharkova1991@gmail.com>
2025-06-05 12:19:47 +0300

    fix status in auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -21,7 +21,7 @@ export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
   res.status(201).json({
-    status: 20,
+    status: 201,
     message: 'Successfully registered a user!',
     data: user,
   });
