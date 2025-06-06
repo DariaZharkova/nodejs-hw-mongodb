ac9969cc444c669fec2082826b27311c5dfa5dc2
Daria Zharkova <dariazharkova1991@gmail.com>
2025-06-06 21:09:10 +0300

    add upload file to cloudinary

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,8 @@ import path from 'node:path';
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
